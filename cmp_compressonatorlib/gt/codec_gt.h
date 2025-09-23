a599efbe2e30f7c8e3287e15436ec6c1be580b99
craftablescience <me@craftablescience.info>
2025-09-23 02:21:07 -0700

    fix: windows build

--- cmp_compressonatorlib/gt/codec_gt.h
+++ cmp_compressonatorlib/gt/codec_gt.h
@@ -62,23 +62,23 @@ public:
     virtual CodecError Compress(CCodecBuffer&       bufferIn,
                                 CCodecBuffer&       bufferOut,
                                 Codec_Feedback_Proc pFeedbackProc = nullptr,
-                                CMP_DWORD_PTR       pUser1        = nullptr,
-                                CMP_DWORD_PTR       pUser2        = nullptr);
+                                CMP_DWORD_PTR       pUser1        = 0,
+                                CMP_DWORD_PTR       pUser2        = 0);
     virtual CodecError Compress_Fast(CCodecBuffer&       bufferIn,
                                      CCodecBuffer&       bufferOut,
                                      Codec_Feedback_Proc pFeedbackProc = nullptr,
-                                     CMP_DWORD_PTR       pUser1        = nullptr,
-                                     CMP_DWORD_PTR       pUser2        = nullptr);
+                                     CMP_DWORD_PTR       pUser1        = 0,
+                                     CMP_DWORD_PTR       pUser2        = 0);
     virtual CodecError Compress_SuperFast(CCodecBuffer&       bufferIn,
                                           CCodecBuffer&       bufferOut,
                                           Codec_Feedback_Proc pFeedbackProc = nullptr,
-                                          CMP_DWORD_PTR       pUser1        = nullptr,
-                                          CMP_DWORD_PTR       pUser2        = nullptr);
+                                          CMP_DWORD_PTR       pUser1        = 0,
+                                          CMP_DWORD_PTR       pUser2        = 0);
     virtual CodecError Decompress(CCodecBuffer&       bufferIn,
                                   CCodecBuffer&       bufferOut,
                                   Codec_Feedback_Proc pFeedbackProc = nullptr,
-                                  CMP_DWORD_PTR       pUser1        = nullptr,
-                                  CMP_DWORD_PTR       pUser2        = nullptr);
+                                  CMP_DWORD_PTR       pUser1        = 0,
+                                  CMP_DWORD_PTR       pUser2        = 0);
 
 private:
     GTCEncodeThreadParam* m_EncodeParameterStorage;
